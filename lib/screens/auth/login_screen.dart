90d06f060a1426a07f79a4014f85b595d88e64d4
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-12 04:30:13 +0530

    Add mobile OTP login entry point

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'phone_login_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const PhoneLoginScreen();
+}
