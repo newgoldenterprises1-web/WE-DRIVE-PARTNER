0f12953ab39b021b7e4274da4f5f37f73a6b3f34
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-13 02:41:01 +0530

    fix widget test expectations for splash screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,12 @@ import 'package:we_drive_partner/main.dart';
 void main() {
   testWidgets('WE DRIVE Partner starts', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    expect(find.text('WE DRIVE Partner'), findsWidgets);
-    expect(find.text('WE DRIVE'), findsWidgets);
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.text('WE DRIVE'), findsOneWidget);
+    expect(
+      find.text('Your Car. Your Comfort. Our Chauffeur.'),
+      findsOneWidget,
+    );
   });
 }
