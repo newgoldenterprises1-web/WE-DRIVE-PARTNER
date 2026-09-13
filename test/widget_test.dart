88072f3aba3fe700f3b7bd638c1e3e5da58bcb34
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-14 05:12:33 +0530

    Fix widget test app class reference

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:we_drive_partner/main.dart';
 
 void main() {
   testWidgets('WE DRIVE Partner starts', (WidgetTester tester) async {
-    await tester.pumpWidget(const WeDrivePartnerApp());
+    await tester.pumpWidget(const MyApp());
     expect(find.text('WE DRIVE'), findsWidgets);
   });
 }
