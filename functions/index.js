27e1fac34614adc51ff8d2dd5ac1e2d9d30834b7
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-17 14:03:50 +0530

    Expose human approval endpoint

--- functions/index.js
+++ functions/index.js
@@ -2,3 +2,4 @@ require('./backend_legacy');
 require('./customer_backend');
 Object.assign(module.exports, require('./payouts'));
 Object.assign(module.exports, require('./ai_gateway'));
+Object.assign(module.exports, require('./ai_approval_http'));
