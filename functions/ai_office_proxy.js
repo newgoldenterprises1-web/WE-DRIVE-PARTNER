ea75a139028f5fb9616d2a038f38b78aa624cafb
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-18 05:50:12 +0530

    Set WE DRIVE AI Office production origin

--- functions/ai_office_proxy.js
+++ functions/ai_office_proxy.js
@@ -7,7 +7,7 @@ const { getAuth } = require('firebase-admin/auth');
 if (!getApps().length) initializeApp();
 
 const MAX_BODY_BYTES = 1024 * 1024;
-const AI_OFFICE_ORIGIN = defineString('AI_OFFICE_ORIGIN', { default: '' });
+const AI_OFFICE_ORIGIN = defineString('AI_OFFICE_ORIGIN', { default: 'https://wedriveai.free.je' });
 const WE_DRIVE_AI_SERVICE_URL = defineString('WE_DRIVE_AI_SERVICE_URL', { default: '' });
 const WE_DRIVE_AI_API_TOKEN = defineSecret('WE_DRIVE_AI_API_TOKEN');
 
