213f3381149b433f8429efe3e3bd5ac9f19eb806
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-24 07:45:25 +0530

    Register notification and verification backend modules

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
 require('./backend_legacy');
+require('./notifications_backend');
+require('./verification_backend');
 require('./customer_backend');
 Object.assign(module.exports, require('./payouts'));
