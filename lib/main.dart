c32e6358a364bb36cb08f67c657e6f2047eebec4
newgoldenterprises1-web <newgoldenterprises1@gmail.com>
2026-09-24 17:11:17 +0530

    Complete partner FCM and app identity

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
 import 'screens/auth/splash_screen.dart';
-import 'theme/app_theme.dart';
 import 'services/push_notification_service.dart';
+import 'theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -28,7 +29,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WE DRIVE Partner',
+      title: 'WeDrive247 Partner',
+      scaffoldMessengerKey: PushNotificationService.scaffoldMessengerKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFFF8FAFC),
